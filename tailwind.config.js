f8e6bee1adf0b1d1e9f78d1c38c6183de3f5afde
Okpainmo <okpainmoandrew@gmail.com>
2024-07-12 17:18:15 +0100

    chore: Tailwindcss setup(with Nativewind) and addition of custom(google) fonts to project

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      // fontSize: {
+      //   heading: ['48px', '64px', '36px', '24px', '18px', '16px'] // h1 to h6
+      // }
+    },
+    screens: {
+      xsm: '450px',
+      // => @media (min-width: 450px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 770px) { ... }
+
+      lg: '1150px',
+      // => @media (min-width: 1150px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+};
