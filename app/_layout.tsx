25dc7df31d2b5aeeab03fda5d5da7d4756e9c7c6
Okpainmo <okpainmoandrew@gmail.com>
2024-07-16 18:44:57 +0100

    chore: configuring the contacts screen on the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -74,6 +74,10 @@ export default function RootLayout() {
         <Stack>
           <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
           <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+          <Stack.Screen
+            name='(user-contacts)'
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name='chat' options={{ headerShown: false }} />
           <Stack.Screen name='+not-found' />
         </Stack>
