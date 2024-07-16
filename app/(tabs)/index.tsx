099aee7911314ea8f28048b7eb5feee5a106bef7
Okpainmo <okpainmoandrew@gmail.com>
2024-07-16 18:42:08 +0100

    refactor: home-page updates

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,17 +8,20 @@ import {
   Platform,
   Image,
   Pressable,
+  TextInput,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   background_variant_1,
   background_variant_2,
   background_variant_1_light,
+  background_variant_3,
+  background_variant_4,
   text_variant_1,
   text_variant_2,
   text_variant_3,
 } from '@/constants/colors';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 const mockAvatar1 = require('../../assets/images/img-8.jpg');
 const mockAvatar2 = require('../../assets/images/img-10.png');
@@ -26,28 +29,93 @@ const mockAvatar3 = require('../../assets/images/img-11.png');
 const mockAvatar4 = require('../../assets/images/img-12.png');
 
 const Home = () => {
+  const [showHeaderSearchTab, setShowHeaderSearchTab] = useState(false);
+  const [showHeaderNavMenu, setShowHeaderNavMenu] = useState(false);
+
   return (
     <SafeAreaView
-      className='flex-1 justify-center items-center flex-col gap-y-4'
+      className='flex flex-1 justify-center items-center flex-col w-full'
       style={{ backgroundColor: background_variant_1_light }}
     >
-      {/* <StatusBar
-        backgroundColor={background_variant_1_light}
-        barStyle={'light-content'}
-      /> */}
-      <View className='flex-1 relative'>
-        <ScrollView
-          className='flex flex-1 min-h-screen'
-          style={styles.safePadding}
+      {/* nav-menu-overlay */}
+      <Pressable
+        onPress={() => {
+          setShowHeaderNavMenu(false);
+        }}
+        className={`nav-menu-overlay flex-1 h-[100%] w-full bg-transparent
+      absolute top-0 left-0 z-40 ${showHeaderNavMenu ? 'flex' : 'hidden'}`}
+      >
+        {/* transparent nav-menu for hiding the nav-menu */}
+      </Pressable>
+      {/* nav-menu */}
+      <View
+        className={`nav-menu absolute right-3 top-[120px] z-50 px-4 pb-4 gap-y-4 
+          rounded-[10px] ${showHeaderNavMenu ? 'flex' : 'hidden'}`}
+        style={{ backgroundColor: background_variant_1_light }}
+      >
+        <Pressable
+          onPress={() => {
+            router.replace('/settings');
+            setShowHeaderNavMenu(false);
+          }}
         >
-          {/* <View
-          className='flex flex-1'
-          style={{ backgroundColor: background_variant_1 }}
-        > */}
+          <Text
+            style={{
+              color: text_variant_1,
+              fontFamily: 'font_300',
+              fontSize: 12,
+            }}
+          >
+            Settings
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            router.replace('/settings');
+            setShowHeaderNavMenu(false);
+          }}
+          className='border-t border-b border-gray-400 py-4'
+        >
+          <Text
+            style={{
+              color: text_variant_1,
+              fontFamily: 'font_300',
+              fontSize: 12,
+            }}
+          >
+            Starred Messages
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            router.replace('/profile');
+            setShowHeaderNavMenu(false);
+          }}
+        >
+          <Text
+            style={{
+              color: text_variant_1,
+              fontFamily: 'font_300',
+              fontSize: 12,
+            }}
+          >
+            Profile
+          </Text>
+        </Pressable>
+      </View>
+      <View
+        className='flex-1 relative w-full pt-[50px]'
+        style={{ backgroundColor: background_variant_1_light }}
+      >
+        <View
+          className='header absolute 
+          left-0 px-3 pt-2 pb-3 w-full top-[40px] z-20'
+          style={{ backgroundColor: background_variant_1_light }}
+        >
           <View
-            className='header flex flex-1 flex-row justify-between items-center w-full 
-        px-3 py-3 mb-[20px]'
-            style={{ backgroundColor: background_variant_1_light }}
+            className={`flex items-center flex-row justify-between pt-3 ${
+              showHeaderSearchTab && 'hidden'
+            }`}
           >
             <Text
               className='text-2xl'
@@ -61,14 +129,78 @@ const Home = () => {
                 size={22}
                 color={text_variant_1}
               />
-              <Ionicons
-                name='search-outline'
-                size={22}
-                color={text_variant_1}
-              />
-              <Ionicons name='grid-outline' size={22} color={text_variant_1} />
+              <Pressable onPress={() => setShowHeaderSearchTab(true)}>
+                <Ionicons
+                  name='search-outline'
+                  size={22}
+                  color={text_variant_1}
+                />
+              </Pressable>
+              <Pressable onPress={() => setShowHeaderNavMenu(true)}>
+                <Ionicons
+                  name='grid-outline'
+                  size={22}
+                  color={text_variant_1}
+                />
+              </Pressable>
             </View>
           </View>
+          <Pressable
+            onPress={() => setShowHeaderSearchTab(false)}
+            className={`hidden z-20 absolute left-3 top-[18px] ${
+              showHeaderSearchTab && 'flex'
+            }`}
+          >
+            <Ionicons
+              name='arrow-back-outline'
+              size={22}
+              color={text_variant_1}
+            />
+          </Pressable>
+          <View
+            className='pl-[35px]'
+            style={{ backgroundColor: `${background_variant_1_light}` }}
+          >
+            <TextInput
+              className={`hidden z-10 top-0 left-0 rounded-full w-full ${
+                showHeaderSearchTab && 'flex'
+              }`}
+              placeholder='find a chat'
+              style={{
+                width: '100%',
+                paddingHorizontal: 20,
+                paddingVertical: 6,
+                backgroundColor: `${background_variant_3}`,
+                borderRadius: 7,
+                color: `${text_variant_1}`,
+                fontSize: 14,
+                fontFamily: 'font_400',
+              }}
+              placeholderTextColor={text_variant_1} // Set the placeholder color here
+              // value={loginForm.email}
+              onChangeText={(text) => {
+                // console.log('email input in progress...');
+                // setLoginForm({
+                //   ...loginForm,
+                //   email: text,
+                // });
+              }}
+            />
+          </View>
+        </View>
+
+        <ScrollView
+          className='flex flex-1 min-h-screen'
+          style={{
+            backgroundColor: background_variant_1,
+            paddingTop: Platform.OS === 'android' ? 65 : 0,
+          }}
+        >
+          {/* <View
+          className='flex flex-1'
+          style={{ backgroundColor: background_variant_1 }}
+        > */}
+
           <View className='chat-types-rack flex flex-row gap-4 px-3'>
             <Text
               className='text-[12px] rounded-full px-4 py-2'
@@ -101,7 +233,7 @@ const Home = () => {
             Pinned
           </Text> */}
           </View>
-          <View className='chats-wrapper mt-4 w-full pb-[100px]'>
+          <View className='chats-wrapper mt-4 w-full pb-[200px]'>
             <Link href='/chat/123' asChild>
               <Pressable className='chat-card px-3 py-3 flex flex-row items-center gap-4'>
                 <Image
@@ -733,9 +865,9 @@ const Home = () => {
           </View>
           {/* </View> */}
         </ScrollView>
-        <Link href='/contacts' asChild>
+        <Link href='/user-contacts' asChild>
           <Pressable
-            className='add-chat-button absolute bottom-[30px] right-4 
+            className='add-chat-button absolute bottom-[15px] right-[15px] 
         flex items-center justify-center w-[60px] h-[60px] rounded-full'
             style={{ backgroundColor: background_variant_2 }}
           >
