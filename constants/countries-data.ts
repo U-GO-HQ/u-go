065ec145837b4f092db5f5d6759c5fc04a67e57b
Okpainmo <okpainmoandrew@gmail.com>
2024-07-19 17:55:20 +0100

    chore: constants additions... added data to aid country-code selection during sign-up

---
+++ constants/countries-data.ts
@@ -0,0 +1,127 @@
+export const countriesData = [
+  {
+    countryName: 'Nigeria',
+    code: '+234',
+    flagUrl: 'https://flagcdn.com/ng.svg',
+  },
+  {
+    countryName: 'United States',
+    code: '+1',
+    flagUrl: 'https://flagcdn.com/us.svg',
+  },
+  {
+    countryName: 'Canada',
+    code: '+1',
+    flagUrl: 'https://flagcdn.com/ca.svg',
+  },
+  {
+    countryName: 'United Kingdom',
+    code: '+44',
+    flagUrl: 'https://flagcdn.com/gb.svg',
+  },
+  {
+    countryName: 'Australia',
+    code: '+61',
+    flagUrl: 'https://flagcdn.com/au.svg',
+  },
+  {
+    countryName: 'Germany',
+    code: '+49',
+    flagUrl: 'https://flagcdn.com/de.svg',
+  },
+  {
+    countryName: 'France',
+    code: '+33',
+    flagUrl: 'https://flagcdn.com/fr.svg',
+  },
+  {
+    countryName: 'Japan',
+    code: '+81',
+    flagUrl: 'https://flagcdn.com/jp.svg',
+  },
+  {
+    countryName: 'India',
+    code: '+91',
+    flagUrl: 'https://flagcdn.com/in.svg',
+  },
+  {
+    countryName: 'Brazil',
+    code: '+55',
+    flagUrl: 'https://flagcdn.com/br.svg',
+  },
+  {
+    countryName: 'South Africa',
+    code: '+27',
+    flagUrl: 'https://flagcdn.com/za.svg',
+  },
+  {
+    countryName: 'China',
+    code: '+86',
+    flagUrl: 'https://flagcdn.com/cn.svg',
+  },
+  {
+    countryName: 'Russia',
+    code: '+7',
+    flagUrl: 'https://flagcdn.com/ru.svg',
+  },
+  {
+    countryName: 'Italy',
+    code: '+39',
+    flagUrl: 'https://flagcdn.com/it.svg',
+  },
+  {
+    countryName: 'Spain',
+    code: '+34',
+    flagUrl: 'https://flagcdn.com/es.svg',
+  },
+  {
+    countryName: 'Mexico',
+    code: '+52',
+    flagUrl: 'https://flagcdn.com/mx.svg',
+  },
+  {
+    countryName: 'South Korea',
+    code: '+82',
+    flagUrl: 'https://flagcdn.com/kr.svg',
+  },
+  {
+    countryName: 'Argentina',
+    code: '+54',
+    flagUrl: 'https://flagcdn.com/ar.svg',
+  },
+  {
+    countryName: 'Egypt',
+    code: '+20',
+    flagUrl: 'https://flagcdn.com/eg.svg',
+  },
+  {
+    countryName: 'Turkey',
+    code: '+90',
+    flagUrl: 'https://flagcdn.com/tr.svg',
+  },
+  {
+    countryName: 'Netherlands',
+    code: '+31',
+    flagUrl: 'https://flagcdn.com/nl.svg',
+  },
+  {
+    countryName: 'Sweden',
+    code: '+46',
+    flagUrl: 'https://flagcdn.com/se.svg',
+  },
+  {
+    countryName: 'Switzerland',
+    code: '+41',
+    flagUrl: 'https://flagcdn.com/ch.svg',
+  },
+  {
+    countryName: 'Saudi Arabia',
+    code: '+966',
+    flagUrl: 'https://flagcdn.com/sa.svg',
+  },
+  {
+    countryName: 'Indonesia',
+    code: '+62',
+    flagUrl: 'https://flagcdn.com/id.svg',
+  },
+];
