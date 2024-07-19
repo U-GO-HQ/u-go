930fdd6f96ff8c396ff4e0524891afce35c37ede
Okpainmo <okpainmoandrew@gmail.com>
2024-07-19 17:57:16 +0100

    chore: Typescript additions for react-native-svg-transformer

--- globals.d.ts
+++ globals.d.ts
@@ -3,3 +3,10 @@
 type ChildProp = {
   children: React.ReactNode;
 };
+
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
